1141f1a3c9a225109c8377e34e147e238bb378fa
KTH <rnjsxogud012@naver.com>
2024-11-27 10:53:42 +0900

    Initial commit

---
+++ C/c언어 과제1/fizzbuzz/Project3/2.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void fizzbuzz(int num);
+
+int main(void)
+{
+	int a;
+	printf("정수를 입력하세요. : ");
+	scanf("%d", &a);
+	fizzbuzz(a);
+	return 0;
+}
+
+void fizzbuzz(int num)
+{
+	int i;
+	for (i = 1; i < num+1; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("fizzbuzz\n");
+		}
+		else if ((i % 3) == 0 && (i % 5) != 0)
+		{
+			printf("fizz\n");
+		}
+		else if ((i % 3) != 0 && (i % 5) == 0)
+		{
+			printf("buzz\n");
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+	}
+}
